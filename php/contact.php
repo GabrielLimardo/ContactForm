c861020b3efcd8241cb59febe5b89b0f88b0b9af
renzoIrusta <renzoirusta22@gmail.com>
2021-06-28 12:57:38 -0300

    terminado contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,45 +1,48 @@
 <?php
 
-if(!$_POST) exit;
+if(!$_POST) exit; //Si no hay un envío por POST no se ejecuta.
 
-// Email address verification, do not edit.
+// Email address verification, do not edit. Verifica que el formato de email sea correcto.
 function isEmail($email) {
 	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
 }
 
-if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n"); //PHP_EOL es sólo un salto de línea. Por lo que averigüé por la web no tiene mucho sentido usarlo, mejor usar \n.
 
+// Separa en variables los datos.
 $name     = $_POST['name'];
 $email    = $_POST['email'];
 $comments = $_POST['comments'];
-
+// Lo que sigue es un validador. Parece haber un error en el primer "if" porque está validando el $name y después requiere un email.
 if(trim($name) == '') {
-	echo '<div class="error_message">Porfavor colocar un email.</div>';
+	echo '<div class="error_message">Porfavor colocar un email.</div>'; //Esto quizás sea un error
 	exit();
 } else if(trim($email) == '') {
-	echo '<div class="error_message">Porfavor colocar un email valido.</div>';
+	echo '<div class="error_message">Porfavor colocar un email valido.</div>'; //Valida que el usuario haya escrito algo en el campo "email"
 	exit();
 } else if(!isEmail($email)) {
-	echo '<div class="error_message">Porfavor colocar un email valido. Intentelo nuevamente.</div>';
+	echo '<div class="error_message">Porfavor colocar un email valido. Intentelo nuevamente.</div>'; //Usa la función isEmail para validar el formato de email.
 	exit();
 }
-
+// Validación que el usuario haya escrito algo en el campo $comments
 if(trim($comments) == '') {
 	echo '<div class="error_message">Porfavor ingresa su mensaje.</div>';
 	exit();
 }
-
+// ???
 if(get_magic_quotes_gpc()) {
 	$comments = stripslashes($comments);
 }
 
+
+// Lo que sigue son configuraciones para el envío automático de emails. Se defienen todas las partes del email en varibles que después son usadas en la función mail(). Si no está bien configurado el servidor todo este código está gastando recursos inútilmente.   
 
 // Configuration option.
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.com";
+$address = "example@example.com"; //Direción del email
 
 
 // Configuration option.
@@ -47,24 +50,26 @@ $address = "example@example.com";
 
 // Example, $e_subject = '$name . ' has contacted you via Your Website.';
 
-$e_subject = 'You have been contacted by ' . $name . '.';
+$e_subject = 'You have been contacted by ' . $name . '.'; //Asunto del email
 
 
 // Configuration option.
 // You can change this if you feel that you need to.
 // Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
-$e_body = "You have been contacted by $name. Their additional message is as follows." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $name via email, $email";
+$e_body = "You have been contacted by $name. Their additional message is as follows." . PHP_EOL . PHP_EOL; //Cuerpo del email
+$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL; //Comentarios del email.
+$e_reply = "You can contact $name via email, $email"; //Tmb se usa en el cuerpo del email.
 
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
-
+$msg = wordwrap( $e_body . $e_content . $e_reply, 70 ); //Función nativa de PHP. Ajusta la cadena de caracteres según un máximo preestablecido. Básicamente le da formato al texto.
+// Formato de la cabecera del email. Todo es un un texto concatenado y con saltos de línea.
 $headers = "From: $email" . PHP_EOL;
 $headers .= "Reply-To: $email" . PHP_EOL;
 $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+//Lo que sigue es un if con la función mail() dentro. La función mail() es nativa de PHP, para que funcione correctamente es necesario configurar el servidor. Si no está configurado el servido esta función se está invocando inútilmente. Si la función no logra mandar el email se ejecuta un echo 'ERROR!'
 
 if(mail($address, $e_subject, $msg, $headers)) {
 
